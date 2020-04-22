92db5a70b434c4ecb8e6f1a85b82a388e03f9ea7
Benjamin Kaduk <bkaduk@akamai.com>
2020-04-22 09:12:36 -0700

    QUIC: Some cleanup for the main QUIC changes
    Try to reduce unneeded whitespace changes and wrap new code to 80 columns.
    Reword documentation to attempt to improve clarity.
    Add some more sanity checks and clarifying comments to the code.
    Update referenced I-D versions.

--- ssl/ssl_ciph.c
+++ ssl/ssl_ciph.c
@@ -2240,6 +2240,7 @@ const char *OSSL_default_ciphersuites(void)
            "TLS_AES_128_GCM_SHA256";
 }
 
+#ifndef OPENSSL_NO_QUIC
 int SSL_CIPHER_get_prf_nid(const SSL_CIPHER *c)
 {
     switch (c->algorithm2 & (0xFF << TLS1_PRF_DGST_SHIFT)) {
@@ -2271,3 +2272,4 @@ int SSL_CIPHER_get_prf_nid(const SSL_CIPHER *c)
     }
     return NID_undef;
 }
+#endif
