1b9330dad82dbb2ef01bc567bd4f6d8964dcfbc4
Tatsuhiro Tsujikawa <404610+tatsuhiro-t@users.noreply.github.com>
2021-03-13 05:37:34 +0900

    QUIC: Process multiple post-handshake messages in a single call (#16)

--- test/sslapitest.c
+++ test/sslapitest.c
@@ -8664,8 +8664,7 @@ static int test_quic_api_version(int clnt, int srvr)
         goto end;
 
     /* Deal with two NewSessionTickets */
-    if (!TEST_true(SSL_process_quic_post_handshake(clientssl))
-            || !TEST_true(SSL_process_quic_post_handshake(clientssl)))
+    if (!TEST_true(SSL_process_quic_post_handshake(clientssl)))
         goto end;
 
     /* Dummy handshake call should succeed */
@@ -8857,8 +8856,7 @@ static int quic_setupearly_data_test(SSL_CTX **cctx, SSL_CTX **sctx,
         return 0;
 
     /* Deal with two NewSessionTickets */
-    if (!TEST_true(SSL_process_quic_post_handshake(*clientssl))
-            || !TEST_true(SSL_process_quic_post_handshake(*clientssl)))
+    if (!TEST_true(SSL_process_quic_post_handshake(*clientssl)))
         return 0;
 
     *sess = SSL_get1_session(*clientssl);
