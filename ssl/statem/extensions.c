934cf1e628404472db529edf8a48a5dcddf79239
Todd Short <tmshort@users.noreply.github.com>
2021-03-18 12:42:01 -0400

    QUIC: Fix CI (#20)
    Fixes #2 and #3 and #22 
    
    Updates `Configure` script to disable QUIC with `no-bulk` and `no-ec`
    Updates build.info doc docs
    Fixes an issue with extension defintions and `no-quic`

--- ssl/statem/extensions.c
+++ ssl/statem/extensions.c
@@ -394,6 +394,7 @@ static const EXTENSION_DEFINITION ext_defs[] = {
     },
 #else
     INVALID_EXTENSION,
+    INVALID_EXTENSION,
 #endif
     {
         /* Must be immediately before pre_shared_key */
