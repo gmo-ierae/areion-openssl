08fe4401375894d7841c7796bb0f9dfce0fa6359
Todd Short <tshort@akamai.com>
2021-05-06 16:24:03 -0400

    QUIC: Break up header/body processing
    As DTLS has changed, so too must QUIC.

--- ssl/statem/statem_quic.c
+++ ssl/statem/statem_quic.c
@@ -11,7 +11,7 @@
 #include "statem_local.h"
 #include "internal/cryptlib.h"
 
-int quic_get_message(SSL *s, int *mt, size_t *len)
+int quic_get_message(SSL *s, int *mt)
 {
     size_t l;
     QUIC_DATA *qd = s->quic_input_data_head;
@@ -19,26 +19,26 @@ int quic_get_message(SSL *s, int *mt, size_t *len)
 
     if (qd == NULL) {
         s->rwstate = SSL_READING;
-        *mt = *len = 0;
+        *mt = 0;
         return 0;
     }
 
     if (!ossl_assert(qd->length >= SSL3_HM_HEADER_LENGTH)) {
         SSLfatal(s, SSL_AD_INTERNAL_ERROR, SSL_R_BAD_LENGTH);
-        *mt = *len = 0;
+        *mt = 0;
         return 0;
     }
 
     /* This is where we check for the proper level, not when data is given */
     if (qd->level != s->quic_read_level) {
         SSLfatal(s, SSL_AD_INTERNAL_ERROR, SSL_R_WRONG_ENCRYPTION_LEVEL_RECEIVED);
-        *mt = *len = 0;
+        *mt = 0;
         return 0;
     }
 
     if (!BUF_MEM_grow_clean(s->init_buf, (int)qd->length)) {
         SSLfatal(s, SSL_AD_INTERNAL_ERROR, ERR_R_BUF_LIB);
-        *mt = *len = 0;
+        *mt = 0;
         return 0;
     }
 
@@ -53,28 +53,32 @@ int quic_get_message(SSL *s, int *mt, size_t *len)
     s->s3.tmp.message_type = *mt = *(s->init_buf->data);
     p = (uint8_t*)s->init_buf->data + 1;
     n2l3(p, l);
-    s->init_num = s->s3.tmp.message_size = *len = l;
+    s->init_num = s->s3.tmp.message_size = l;
     s->init_msg = s->init_buf->data + SSL3_HM_HEADER_LENGTH;
 
+    return 1;
+}
+
+int quic_get_message_body(SSL *s, size_t *len)
+{
     /* No CCS in QUIC/TLSv1.3? */
-    if (*mt == SSL3_MT_CHANGE_CIPHER_SPEC) {
+    if (s->s3.tmp.message_type == SSL3_MT_CHANGE_CIPHER_SPEC) {
         SSLfatal(s, SSL_AD_UNEXPECTED_MESSAGE, SSL_R_CCS_RECEIVED_EARLY);
         *len = 0;
         return 0;
     }
     /* No KeyUpdate in QUIC */
-    if (*mt == SSL3_MT_KEY_UPDATE) {
+    if (s->s3.tmp.message_type == SSL3_MT_KEY_UPDATE) {
         SSLfatal(s, SSL_AD_UNEXPECTED_MESSAGE, SSL_R_UNEXPECTED_MESSAGE);
         *len = 0;
         return 0;
     }
 
-
     /*
      * If receiving Finished, record MAC of prior handshake messages for
      * Finished verification.
      */
-    if (*mt == SSL3_MT_FINISHED && !ssl3_take_mac(s)) {
+    if (s->s3.tmp.message_type == SSL3_MT_FINISHED && !ssl3_take_mac(s)) {
         /* SSLfatal() already called */
         *len = 0;
         return 0;
@@ -108,5 +112,6 @@ int quic_get_message(SSL *s, int *mt, size_t *len)
                         (size_t)s->init_num + SSL3_HM_HEADER_LENGTH, s,
                         s->msg_callback_arg);
 
+    *len = s->init_num;
     return 1;
 }
