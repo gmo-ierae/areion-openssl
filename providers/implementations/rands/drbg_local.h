7530b05f5e8222915ed96c16603195a08bff1a17
Rich Salz <rsalz@akamai.com>
2023-05-23 15:18:52 -0400

    Incorporate DRBG locking enhancements
    commit 040ea69fb5a7f3f4bdbb4588767aef107ca773b6
    Author: Matt Caswell <matt@openssl.org>
    Date:   Mon May 15 15:30:10 2023 +0100
    
        Enable obtaining certain DRBG params without a lock
    
        Even if a DRBG has locking enabled on it, there are certain parameters
        which are still safe to obtain even without a lock. The max_request
        value is constant for all our DRBGs. The reseed_counter does not matter
        if we get it wrong - so it is safe to avoid the lock.
    
        Partially fixes #20286
    
    commit 726539565ec38d2a4052614e01a4ea88f0266e4c
    Author: Matt Caswell <matt@openssl.org>
    Date:   Mon May 15 11:33:01 2023 +0100
    
        Refactor the DRBG implementations to manage locking themselves
    
        Previously the EVP layer would call lock and unlock functions on the
        underlying DRBG implementation to say when a lock should be acquired and
        released. This gives the DRBG implementation no say as to what kind of
        lock should obtained (e.g. read/write) or even whether a lock is actually
        needed or not.
    
        In reality we know whether a DRBG is supposed to be in locking mode or
        not because the "enable_locking()" function will have been called if
        locks should be used. Therefore we re-interpret the lock and unlock
        functions as "hints" from the EVP layer which we ignore. Instead we
        acquire locks only when we need them. By knowing the context we can obtain
        either a read or a write lock as appropriate.
    
        This may mean that in some rare cases we acquire the locks more than once
        for a single EVP call, if the EVP call makes several calls to the underlying
        DRBG. But in practice almost all EVP calls only make one such call.
        EVP_RAND_generate() is an example of a call where multiple DRBG calls may
        be made. One of these gets the "max_request" parameter (which is constant
        for all of our own DRBGs) and it may make several calls to the DRBG generate
        call - but only if the requested size is very large which will rarely be
        the case.
    
        Partially fixes #20286

--- providers/implementations/rands/drbg_local.h
+++ providers/implementations/rands/drbg_local.h
@@ -212,7 +212,7 @@ OSSL_FUNC_rand_clear_seed_fn ossl_drbg_clear_seed;
                                             \
         for (i = 0; i < OSSL_NELEM(v); i++) \
             if ((v)[i] != 0)                \
-                return 0;                   \
+                goto err;                   \
     }
 
 /* locking api */
@@ -222,6 +222,8 @@ OSSL_FUNC_rand_unlock_fn ossl_drbg_unlock;
 
 /* Common parameters for all of our DRBGs */
 int ossl_drbg_get_ctx_params(PROV_DRBG *drbg, OSSL_PARAM params[]);
+int ossl_drbg_get_ctx_params_no_lock(PROV_DRBG *drbg, OSSL_PARAM params[],
+                                     int *complete);
 int ossl_drbg_set_ctx_params(PROV_DRBG *drbg, const OSSL_PARAM params[]);
 
 #define OSSL_PARAM_DRBG_SETTABLE_CTX_COMMON                                      \
